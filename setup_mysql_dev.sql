98c46aed0d5808a42b9202ee7cec8723efca2c4b
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-27 22:40:41 +0000

    testing issues

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,13 @@
+-- prepares a MySQL server for the project database hbnb_dev_db
+-- new user hbnb_dev in localhost
+-- password of hbnb_dev set to hbnb_dev_pwd
+-- user should have all privileges on hbnb_dev_db
+-- user should have SELECT privileges on database performance_schema
+-- if the database hbnb_dev_db or user hbnb_dev already exist script shouldnt
+-- fail
+
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS 'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON 'performance_schema'.* TO 'hbnb_dev'@'localhost';
+FLUSH PRIVILEGES;
