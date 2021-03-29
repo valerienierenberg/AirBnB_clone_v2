14fed2ef43e25b7d8b36d7ff601f9f1b361ab55d
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-29 18:49:24 +0000

    updated class

--- models/amenity.py
+++ models/amenity.py
@@ -3,5 +3,11 @@
 from models.base_model import BaseModel
 
 
-class Amenity(BaseModel):
-    name = ""
+class Amenity(BaseModel, Base):
+    __tablename__= 'amenities'
+    name = Column(String(128), nullable=False)
+""" class attribute place_amenities must represent a relationship Many-To-Many
+between the class Place and Amenity. Please see below more detail: place_amenity
+ in the Place update """
+
+    place_amenities =
