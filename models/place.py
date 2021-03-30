84208027a7f87b9dc4fb60f5d85a218d49682df4
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-30 18:15:30 +0000

    import Base and fix syntax error

--- models/place.py
+++ models/place.py
@@ -1,13 +1,13 @@
 #!/usr/bin/python3
 """ Place Module for HBNB project """
-from models.base_model import BaseModel
+from models.base_model import BaseModel, Base
 
 place_amenity = Table('place_amenity', Base.metadata,
                       Column('place_id', String(60),
-                             ForeignKey('places.id' nullable=False),
+                             ForeignKey('places.id'), nullable=False,
                              primary_key=True),
                       Column('amenity_id', String(60),
-                             ForeignKey('amenities.id' nullable=False),
+                             ForeignKey('amenities.id'), nullable=False,
                              primary_key=True))
 
 class Place(BaseModel, Base):
