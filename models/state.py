dab78fd362423983884c886ce28579a7c02384e9
valerienierenberg <valerie.nierenberg@gmail.com>
2021-03-30 19:20:57 +0000

    Add import relationship and uncomment relationship line

--- models/state.py
+++ models/state.py
@@ -2,13 +2,14 @@
 """ State Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
+from sqlalchemy.orm import relationship
 
 
 class State(BaseModel, Base):
     """ State class """
     __tablename__ = 'states'
     name = Column(String(128), nullable=False)
-#    cities = relationship("City", backref="state")
+    cities = relationship("City", backref="state")
 
     @property
     def cities(self):
