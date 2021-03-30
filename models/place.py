706a84029b13e2c7879ed0b91720b057df357612
blanketmanatee <ashleyl.welch@gmail.com>
2021-03-30 17:52:09 +0000

    done task 10 until update place class

--- models/place.py
+++ models/place.py
@@ -2,8 +2,15 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel
 
+place_amenity = Table('place_amenity', Base.metadata,
+                      Column('place_id', String(60),
+                             ForeignKey('places.id' nullable=False),
+                             primary_key=True),
+                      Column('amenity_id', String(60),
+                             ForeignKey('amenities.id' nullable=False),
+                             primary_key=True))
 
-class Place(BaseModel):
+class Place(BaseModel, Base):
     """ A place to stay """
     city_id = ""
     user_id = ""
