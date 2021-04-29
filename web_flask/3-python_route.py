47df796d9be72f6ddccc1cb1139f88d4afa5e9ef
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-29 16:30:46 +0000

    Add task 3

---
+++ web_flask/3-python_route.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+""" this module contains a script that starts a Flask web application
+    the web application must be listening on 0.0.0.0, port 5000
+    Routes:
+    /
+    /hbnb
+    /c/<text>
+    /python/(<text>)
+"""
+from flask import Flask
+app = Flask(__name__)
+
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """ route to return Hello HBNB! """
+    return 'Hello HBNB!'
+
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb_only():
+    """ route to return HBNB """
+    return 'HBNB'
+
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text_var(text):
+    """ route to return C followed by text variable, replaces _ with spaces """
+    text = text.replace('_', ' ')
+    return ('C' + ' ' + text)
+
+
+@app.route('/python', strict_slashes=False)
+@app.route('/python/<path:text>', strict_slashes=False)
+def python_iscool(text=None):
+    """ route to return text followed by "is cool" by default, (which can be
+    overwritten), replaces _ with spaces """
+    if text is None:
+        text = 'is cool'
+    else:
+        text = text.replace('_', ' ')
+    return ('Python' + ' ' + text)
+
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
