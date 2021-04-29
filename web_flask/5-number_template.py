94fecaa4ac9fe87bb9d114df3aff0f5ed0dbbb52
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-29 19:24:27 +0000

    Add task 5 and modify comment on 4

--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -7,8 +7,9 @@
     /c/<text>
     /python/(<text>)
     /number/<int:n>
+    /number_template/<int:n>
 """
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
@@ -47,6 +48,12 @@ def python_iscool(text=None):
 def number(n):
     """ route that displays “n is a number” only if n is an integer """
     return ("{:d} is a number".format(n))
+
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """ route with H1 tag: “Number: n” inside the tag BODY """
+    return render_template('5-number.html', n=n)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
