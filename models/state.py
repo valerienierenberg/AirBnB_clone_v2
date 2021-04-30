4adb68a27630e30ad2b701af0e87ddce2e468f24
valerienierenberg <valerie.nierenberg@gmail.com>
2021-04-30 17:08:57 +0000

    Specify getter only for if storage is db

--- models/state.py
+++ models/state.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 """ State Module for HBNB project """
+import models
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, String, Integer, ForeignKey
 from sqlalchemy.orm import relationship
+from models.city import City
 
 
 class State(BaseModel, Base):
@@ -11,16 +13,17 @@ class State(BaseModel, Base):
     name = Column(String(128), nullable=False)
     cities = relationship("City", backref="state")
 
-    @property
-    def cities(self):
-        """getter for cities (for FileStorage)
-        Returns:
-        list of City instances with state_id equal to the current State.id
-        """
-        cities = []
-        allcities = models.storageg.all(City)
-        for city in allcities.values():  # values() method returns an obj
-            # that contains the values of the dictionary as a list.
-            if city.state_id == self.id:
-                cities.append(city)
-        return cities
+    if models.storage != "db":
+        @property
+        def cities(self):
+            """getter for cities (for FileStorage)
+            Returns:
+            list of City instances with state_id equal to the current State.id
+            """
+            cities = []
+            allcities = models.storage.all(City)
+            for city in allcities.values():  # values() method returns an obj
+                # that contains the values of the dictionary as a list.
+                if city.state_id == self.id:
+                    cities.append(city)
+            return cities
